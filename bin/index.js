08a523cc6c45e713f57ba3a02b6f1bbd7e6d9a27
Jason <jason050196@gmail.com>
2022-03-26 19:17:53 +0800

    Update index.js

--- bin/index.js
+++ bin/index.js
@@ -396,7 +396,7 @@ async function generateImages() {
         });
         seen.push(images);
         const b64 = await mergeImages(images, { Canvas: Canvas, Image: Image });
-        await ImageDataURI.outputFile(b64, outputPath + `${id}.gif`);
+        await ImageDataURI.outputFile(b64, outputPath + `${id}.png`);
         images = [];
         id++;
       }
@@ -410,7 +410,7 @@ async function generateImages() {
       });
       generateMetadataObject(id, images);
       const b64 = await mergeImages(images, { Canvas: Canvas, Image: Image });
-      await ImageDataURI.outputFile(b64, outputPath + `${id}.gif`);
+      await ImageDataURI.outputFile(b64, outputPath + `${id}.png`);
       images = [];
       id++;
     }
